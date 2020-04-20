e953d1de99e6b2612df96ecb3f9201cb6e8fe2e4
controlapy <contralor@controlapy.org>
2020-04-19 23:22:11 -0400

    CSS previo al lanzamiento !

--- beneficiario/models.py
+++ beneficiario/models.py
@@ -25,3 +25,53 @@ class Pytyvo(models.Model):
 
     class Meta:
       verbose_name_plural = "pytyvos"
+
+class IPS(models.Model):
+    cedula = models.CharField(max_length=255, unique=True)
+    nombreApellido = models.CharField(max_length=512)
+
+    class Meta:
+      verbose_name_plural = "ipss"
+
+class SET(models.Model):
+    cedula = models.CharField(max_length=255, unique=True)
+    nombreApellido = models.CharField(max_length=512)
+
+    class Meta:
+      verbose_name_plural = "sets"
+
+class SFP(models.Model):
+    cedula = models.CharField(max_length=255, unique=True)
+    nombreApellido = models.CharField(max_length=512)
+    departamento = models.CharField(max_length=255,blank=True,null=True)
+    distrito = models.CharField(max_length=255,blank=True,null=True)
+
+    class Meta:
+      verbose_name_plural = "sfps"
+
+class MJ(models.Model):
+    cedula = models.CharField(max_length=255, unique=True)
+    nombreApellido = models.CharField(max_length=512)
+    departamento = models.CharField(max_length=255,blank=True,null=True)
+    distrito = models.CharField(max_length=255,blank=True,null=True)
+
+    class Meta:
+      verbose_name_plural = "mjs"
+
+class Jubilado(models.Model):
+    cedula = models.CharField(max_length=255, unique=True)
+    nombreApellido = models.CharField(max_length=512)
+    departamento = models.CharField(max_length=255,blank=True,null=True)
+    distrito = models.CharField(max_length=255,blank=True,null=True)
+
+    class Meta:
+      verbose_name_plural = "jubilados"
+
+class Tekopora(models.Model):
+    cedula = models.CharField(max_length=255, unique=True)
+    nombreApellido = models.CharField(max_length=512)
+    departamento = models.CharField(max_length=255,blank=True,null=True)
+    distrito = models.CharField(max_length=255,blank=True,null=True)
+
+    class Meta:
+      verbose_name_plural = "tekoporas"
--- beneficiario/views.py
+++ beneficiario/views.py
@@ -2,6 +2,12 @@ from django.utils import timezone
 from .models import Beneficiario
 from .models import Nangareko
 from .models import Pytyvo
+from .models import IPS
+from .models import SET
+from .models import SFP
+from .models import MJ
+from .models import Jubilado
+from .models import Tekopora
 from django.shortcuts import render, get_object_or_404
 from .forms import BeneficiarioForm
 from django.shortcuts import redirect
@@ -37,9 +43,49 @@ def buscar(request):
     if request.method == "POST":
         cedula = request.POST.get("cedula","")
         print(cedula)
-        nangareko = get_object_or_404(Nangareko, cedula=cedula)
-        pytyvo = get_object_or_404(Pytyvo, cedula=cedula)
-        return render(request, 'beneficiario/detail.html', {'nangareko': nangareko, 'pytyvo': pytyvo})
+        try:
+            nangareko = Nangareko.objects.get(cedula=cedula)
+        except Nangareko.DoesNotExist:
+            nangareko = None
+        try:
+            pytyvo = Pytyvo.objects.get(cedula=cedula)
+        except Pytyvo.DoesNotExist:
+            pytyvo = None
+        try:
+            ips = IPS.objects.get(cedula=cedula)
+        except IPS.DoesNotExist:
+            ips = None
+        try:
+            set = SET.objects.get(cedula=cedula)
+        except SET.DoesNotExist:
+            set = None
+        try:
+            sfp = SFP.objects.get(cedula=cedula)
+        except SFP.DoesNotExist:
+            sfp = None
+        try:
+            mj = MJ.objects.get(cedula=cedula)
+        except MJ.DoesNotExist:
+            mj = None
+        try:
+            tekopora = Tekopora.objects.get(cedula=cedula)
+        except Tekopora.DoesNotExist:
+            tekopora = None
+        try:
+            jubilado = Jubilado.objects.get(cedula=cedula)
+        except Jubilado.DoesNotExist:
+            jubilado = None
+        return render(request, 'beneficiario/detail.html', 
+            {
+                'nangareko': nangareko, 
+                'pytyvo': pytyvo,
+                'ips': ips, 
+                'set': set,
+                'sfp': sfp,
+                'mj': mj,
+                'tekopora': tekopora,
+                'jubilado': jubilado
+            })
 
 #            return redirect('beneficiario/detail/', {'cedula': cedula})
 #
@@ -47,5 +93,3 @@ def buscar(request):
     else:
         form = BeneficiarioForm()
     return render(request, 'beneficiario/buscar.html', {'form': form})
-
-
